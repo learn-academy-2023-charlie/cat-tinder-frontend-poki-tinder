836e6e8b13fc04d9c031281b620b2b2005628e27
henrike32 <henrique.lambertucci@gmail.com>
2023-06-08 18:39:44 -0400

    done test-coverage for home and NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 const NotFound = () => {
-    return(
-      <>
-       <h1> Not Found </h1>
-      </>
-    )
+  return (
+    <>
+      <h1> Not Found </h1>
+    </>
+  )
 }
 export default NotFound
