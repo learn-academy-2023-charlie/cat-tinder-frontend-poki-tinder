407023cf975a50a5a1ccd04d07a2d7d756a3b473
henrike32 <henrique.lambertucci@gmail.com>
2023-06-08 18:39:44 -0400

    done test-coverage for home and NotFound

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react"
-import Home from "../Home"
+import Home from "../pages/Home"
 
 describe("<Home />", () => {
   it("renders without crashing", () => {
